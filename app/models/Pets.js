987a37fb6f5edbceb529df88ccfee445ea7dcc39
kirenia <206641702+kirenia@users.noreply.github.com>
2026-07-13 20:28:22 -0600

    validation

--- app/models/Pets.js
+++ app/models/Pets.js
@@ -5,14 +5,21 @@ const petSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Pet name is required"],
+    maxlength: [50, "Pet name cannot exceed 50 characters"],
   },
   type: {
     type: String,
     required: [true, "Pet type is required"],
+    enum: {
+      // use enum to restrict the values of the type field to a specific set of values
+      values: ["Dog", "Cat", "Bird", "Other"],
+      message: "Pet type must be either Dog, Cat, Bird, or Other",
+    },
   },
   age: {
     type: Number,
     required: [true, "Pet age is required"],
+    min: [0, "Pet age cannot be negative"],
   },
 });
 
