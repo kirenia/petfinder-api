fe445a73f068d6c06ee0978145cd805b7c49a7d6
kirenia <206641702+kirenia@users.noreply.github.com>
2026-07-26 16:16:25 -0600

    enable cors

--- server/app/index.js
+++ server/app/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const routes = require("./routes/index");
+const cors = require("cors"); // so client can make requests to the server
+
+app.use(cors()); // enable CORS for all routes
 
 app.use(express.json());
 app.use(morgan("dev")); // use morgan middleware for logging HTTP requests in development mode
